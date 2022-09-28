bdff1b50c44e5be3332b7cceb2e7409fe3d09502
Chau Trinh <trinhchau.1006@gmail.com>
2022-09-28 15:23:59 +0700

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+export {HomeHeader,NFTCard,FocusedStatusBar,CircleButton,RectButton, DetailsBid, DetailsDesc, SubInfo}
